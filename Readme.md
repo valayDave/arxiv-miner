67bdc22a69ba03a38d3c3df5a267d9343508b4bd
Valay Dave <valaygaurang@gmail.com>
2020-06-23 23:15:27 -0700

    Arxiv Miner is successfully able to part 39/58 samples. testing variations of the algorithm.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,30 @@
 # Arxiv Miner. 
 
 Repository Helps Mine Arxiv Papers to quickly Scrape through new Papers and Mine data for Faster Readings. 
+
+## What is Done Yet : 
+
+1. Arxiv PDF and LateX Extraction Pipeline
+2. Arxiv Paper Parsing to JSON Objects using Latex and Python. 
+
+
+## BROADER GOAL
+1. The goal of this project is to annotate and build faster search around research papers so that I can be quickly aware of what is happening in the domain. 
+2. It is also ment to structure research papers in searialisable JSON so that I can start annotating research and fixing things around the same. 
+
+# How can One get there ?
+
+## ARXIV PAPER MINING
+
+### GOAL OF PAPER MINING 
+Parse the Arxiv Latex/PDF into A research Paper Object which can be serialised so that It is in readable format for some form of Machine learning/Annoation methods. But it all starts from cleaning the Dirt from Arxiv. 
+
+### WAY TO DO IT 
+1. Extract Papers from `Arxiv` using Adjrej karapathy's `fetch_papers.py` script. It fills the `db.p` Pickle File.
+2. `arxiv_multiprocess_download.py` will download the Latex version of the Papers for Arxiv and create and `ArxivPaper` object.  
+3. The `ArxivPaper` Object created extracts the Latex source from the Arxiv. **Integration is TODO**
+    - Three things will help solve the Information mining Problem. 
+        1. Extraction of Document Structure/hierarchy via Python-Latex Libraries like `tex2py`. 
+        2. Extraction of Text from Latex Document Using `detex` : https://github.com/pkubowicz/opendetex
+        3. Collate with the Tree with the text based on hierachical traversal of tree and text-splittig based search to collate the information. 
+    - These things are Managed using the child classes of `LatexInformationParser`. These child classes will help for the Structured `Section` objects which contains the stored parsed structure of the Research paper. 
--- arxiv_miner.py
+++ arxiv_miner.py
@@ -5,22 +5,39 @@ import os
 import tarfile
 import glob
 import json
+from parse_paper import get_tex_tree,LatexInformationParser,LatexParserException,MaxSectionSizeException,Section,split_match
+import datetime
+from typing import List
+
+# from subprocess import Popen, PIPE
+
+# def clean_latex_comments(document_path):
+#     process = Popen(["./latex_expand", document_path], stdout=PIPE)
+#     (output, err) = process.communicate()
+#     exit_code = process.wait()
+#     return output
 
 class ArxivPaperMeta():
     
     def __init__(self,\
                 pdf_only = False,\
                 latex_files = 0,\
-                mined = True):
+                mined = True,\
+                latex_parsable=True,\
+                updated_on=datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")):
         self.pdf_only = pdf_only
         self.latex_files = latex_files
         self.mined = mined
+        self.latex_parsable=latex_parsable
+        self.updated_on=updated_on
 
     def to_json(self):
         return dict(
             pdf_only =self.pdf_only,\
             latex_files = self.latex_files,\
             mined = self.mined,\
+            latex_parsable = self.latex_parsable,\
+            updated_on = self.updated_on
         )
 
 class ArxivLoader():
@@ -39,7 +56,6 @@ class ArxivLoader():
         for paper in self.papers:
             object_array.append(paper.core_meta)
         return object_array
-
 
 class ArxivPaper(object):
     """ArxivPaper 
@@ -64,6 +80,7 @@ class ArxivPaper(object):
         else:
             self._buid_from_fs()
         # scan for the presence of the object in the FS.
+
     @property
     def arxiv_meta_file_path(self):
         return os.path.join(self.paper_root_path,self.arxiv_save_file_name)
@@ -71,6 +88,11 @@ class ArxivPaper(object):
     @property
     def paper_meta_file_path(self):
         return os.path.join(self.paper_root_path,self.paper_meta_save_file_name)
+
+    @property
+    def tex_files(self):
+        file_names = list(glob.glob(os.path.join(self.latex_root_path,"*.tex")))
+        return file_names
 
     def _load_metadata_from_fs(self):
         self.arxiv_object = load_json_from_file(self.arxiv_meta_file_path)
@@ -101,16 +123,18 @@ class ArxivPaper(object):
         Published : {published}
         
 
-        Analytics
+        MetaData
         ---------
         pdf_only = {pdf_only}
+        latex_parsable = {latex_parsable}
+        updated_on = {updated_on}
         latex_files = {latex_files}
         mined = {mined}
         '''.format(**self.core_meta)
         return format_str
 
     def _buid_from_fs(self):
-         self._load_metadata_from_fs()
+        self._load_metadata_from_fs()
 
     def _build_paper(self):
         """_build_paper 
@@ -127,12 +151,112 @@ class ArxivPaper(object):
         with tarfile.open(downloaded_data) as tar:
             tar.extractall(path=self.latex_root_path)
         
-        file_names = list(glob.glob(os.path.join(self.latex_root_path,"*.tex")))
         # $ Set some Common Meta WRT the Paper. 
         self.paper_meta = ArxivPaperMeta()
-        self.paper_meta.latex_files = len(file_names)
-        self.paper_meta.pdf_only = True if len(file_names) == 0 else False
+        self.paper_meta.latex_files = len(self.tex_files)
+        self.paper_meta.pdf_only = True if len(self.tex_files) == 0 else False
         # $ Remove the Tar File.
         os.remove(downloaded_data)
         # $ Save the Metadata
         self._save_metadata_to_fs()
+
+class SingleDocumentLatexParser(LatexInformationParser):
+    def __init__(self, max_section_limit=20,detex_path=None):
+        super().__init__(max_section_limit=max_section_limit,detex_path=detex_path)
+    
+    def section_extraction(self,tex_file_path) -> List[Section]:
+        tex_node = get_tex_tree(tex_file_path)
+        if len(tex_node.branches) > self.max_section_limit:
+            raise MaxSectionSizeException()
+        
+        sequential_sections = []
+        for node in tex_node:
+            curr_section = Section(str(node))
+            subsections = self.get_subsection_names(node)
+            curr_section.subsections = [Section(ss) for ss in subsections]
+            sequential_sections.append(curr_section)
+
+        return sequential_sections
+    
+    def text_extraction(self,latex_path):
+        return self.text_extractor(latex_path)
+
+            
+    @staticmethod
+    def split_and_find_section(curr_text,curr_sec_name,prev_section,split_upto=0.2,split_bins=10):
+        """split_and_find_section 
+        Helps Recurrsively/iteratively split a Latex document for the Section list found from 
+        `section_extraction`. Splits a text string based on `curr_sec_name` and then allocates the 
+        split[0] to the `prev_section` object. 
+        
+        :type curr_text: [String]
+        :type curr_sec_name: [String]
+        :type prev_section: [Section]
+        :type split_upto : refer `split_match`
+        :type split_bins : refer `split_match`
+        :returns curr_text,Find_status : After removal of redundant section post splitting. 
+                                         status points to weather it could set text of the 
+                                         `prev_section` object
+        """
+        current_text_split = split_match(curr_sec_name,curr_text,split_upto=split_upto,split_bins=split_bins)
+        # print("Found Splits,",curr_sec_name,len(current_text_split))
+        if len(current_text_split) == 0: 
+            # This means no splits were found 
+            return curr_text,False
+
+        portion_before_section = current_text_split[0] 
+
+        if prev_section is not None:
+            prev_section.text = portion_before_section
+            # print(ss.name,"added To Section ",prev_section.name,len(prev_section.text))
+        portion_after_section = current_text_split[1:]
+        curr_text = ''.join(portion_after_section)
+        return curr_text,True
+
+    
+    def collate_sections(self,paper_text,section_list:List[Section],split_upto=0.2,split_bins=10):
+        """collate_sections 
+        Gets Latex compiled text string and 
+        then uses the found section based hierarchy from Latex
+        to fill Text content of the `Section` objects which were discovered in 
+        `section_extraction`
+
+        :param paper_text: text in string of from text_extraction]
+        :type section_list: List[Section]
+        :return: List[Section] : filled with text attributed
+        """
+        current_text_split = []
+        prev_section = None
+        curr_text = str(paper_text)
+        unfound_sections = []
+        some_section_not_found = False
+        for index,s in enumerate(section_list):
+            curr_text,section_status = self.split_and_find_section(curr_text,s.name,prev_section,split_upto=split_upto,split_bins=split_bins)
+            if not section_status: # If couldn't match section add it here. 
+                some_section_not_found = True
+            # print('\n\t'+s.name)                
+            prev_section = s 
+            for ss in s.subsections:
+                curr_text,section_status = self.split_and_find_section(curr_text,ss.name,prev_section,split_upto=split_upto,split_bins=split_bins)
+                if not section_status:
+                    some_section_not_found = True
+                    # print("Cannot Match For :",ss.name)
+                prev_section = ss
+                # print('\n\t\t'+ss.name)
+            if index == len(section_list)-1:
+                s.text = curr_text
+        return section_list,some_section_not_found
+    
+    def from_arxiv_paper(self,paper:ArxivPaper,lowest_section_match_percent=0.2,number_to_tries=10):
+        """from_arxiv_paper 
+        Extract Parsable section array from Arxiv Latex Paper which only have one paper will all the sections in it. 
+        :param paper: [ArxivPaper]
+        :param lowest_section_match_percent [float]: % of the section heading that mimimum matches to create the split in text for parsing. 
+        :param number_to_tries: [float], number of different matches to make with mimimum match strings
+        :return: List[Section],bool
+        """
+        latex_path = paper.tex_files[0]
+        sections = self.section_extraction(latex_path)
+        tex_in_text = self.text_extraction(latex_path)
+        sections,some_section_not_found = self.collate_sections(tex_in_text,sections,split_upto=lowest_section_match_percent,split_bins=number_to_tries)
+        return sections,some_section_not_found
