86f879bcbbea8ed668e81b34e1aa223d67beccac
Valay Dave <valaygaurang@gmail.com>
2021-01-19 17:55:26 -0700

    Fix in ont miner.

--- arxiv_miner/ontology_miner.py
+++ arxiv_miner/ontology_miner.py
@@ -24,7 +24,7 @@ class OntologyMiner:
         return ontology
 
     @staticmethod
-    def mine_lots_of_papers(records:List[ArxivSematicParsedResearch]):
+    def mine_lots_of_papers(records:List[ArxivSematicParsedResearch],workers=1):
         ontologies = []
         try:
             mine_dict = {}
@@ -32,7 +32,10 @@ class OntologyMiner:
             for r in records:
                 mine_dict[r.identity.identity] = {"title":r.identity.title,"abstract":r.identity.abstract}
                 id_dict[r.identity.identity] = r
-            cso_ontology = CSO.run_cso_classifier_batch_model_single_worker(mine_dict)
+            if workers == 1:
+                cso_ontology = CSO.run_cso_classifier_batch_model_single_worker(mine_dict)
+            else:
+                cso_ontology = CSO.run_cso_classifier_batch_mode(mine_dict,workers=workers)
             for ontid in cso_ontology:
                 identity = id_dict[ontid]
                 ontology = Ontology(
