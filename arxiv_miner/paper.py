3a3c7441988b47e45fa479e23fd8964630da442f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-03 23:20:02 +0000

    Adding numeric_owner as keyword arguement

--- arxiv_miner/paper.py
+++ arxiv_miner/paper.py
@@ -234,7 +234,7 @@ class ArxivPaper(ArxivRecord):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=self.latex_root_path)
