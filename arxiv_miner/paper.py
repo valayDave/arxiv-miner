59d9d9105f494db601aa2aaa6f548a84dbaf2989
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2024-03-23 15:13:38 -0500

    CVE-2007-4559 Patch (#12)
    * Adding tarfile member sanitization to extractall()
    
    * Adding numeric_owner as keyword arguement
    
    ---------
    
    Co-authored-by: TrellixVulnTeam <kasimir.schulz@trellix.com>

--- arxiv_miner/paper.py
+++ arxiv_miner/paper.py
@@ -218,7 +218,26 @@ class ArxivPaper(ArxivRecord):
             raise ArxivAPIException(self.paper_id,str(e))
         # $ Extract Files in Folder.
         with tarfile.open(downloaded_data) as tar:
-            tar.extractall(path=self.latex_root_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self.latex_root_path)
 
         # $ Remove the Tar File.
         if not store_latex:
